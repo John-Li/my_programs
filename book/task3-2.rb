936f979b2b4148e3ebdd2286ad22c2d97f1dc963
John Li <ivan.lishchinovskiy@gmail.com>
2011-11-29 16:14:04 +0200

    task 3-2 fixed

--- book/task3-2.rb
+++ book/task3-2.rb
@@ -1,18 +1,13 @@
 #Задача 2.
 #Даны три действительных числа X, Y, Z. Найти минимальное из этих трех чисел.
 
-puts "Please enter three numbers to find which is the smallest one"
-puts "Number 1 is:"
-a = gets.to_i
-puts "Number 2 is:"
-b = gets.to_i
-puts "Number 3 is:"
-c = gets.to_i
+a = []
 
-if a < b and a < c
-  puts "First number is the smallest"
-elsif b < a and b < c 
-  puts "Second number is the smallest"
-elsif c < a and c < b 
-  puts "Third number is the smallest"
-end
+puts "Please enter three numbers to find which is the smallest one. \nNumber 1 is:"
+a.push(gets.to_i)
+puts "Number 2 is:"
+a.push(gets.to_i)
+puts "Number 3 is:"
+a.push(gets.to_i)
+
+puts "This #{a.min} is the smallest one!"
