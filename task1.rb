240dda4a145091e2caaf00e635cb9c5c0f689c4b
John Li <ivan.lishchinovskiy@gmail.com>
2011-11-29 14:14:30 +0200

    task1 fixed

--- task1.rb
+++ task1.rb
@@ -1,11 +1,12 @@
-puts "Please enter two integers to find out which is bigger one"
-puts "The first integer is:"
+puts "Please enter two integers to find out which is bigger one. \nThe first integer is:"
 a = gets.to_i 
 puts "The second integer is:"
 b = gets.to_i
 
-if a > b
-puts "The first integer is bigger than the second."
+result = if a > b
+  "The first integer is bigger than the second."
 else
-puts "The second integer is bigger than the first."
+  "The second integer is bigger than the first."
 end
+
+puts result
