9e5f75579622c798060b7eed58d5075d8f6b58f5
John Li <ivan.lishchinovskiy@gmail.com>
2011-11-29 14:05:52 +0200

    task 4 using arr was fixed

--- task4-1.rb
+++ task4-1.rb
@@ -3,15 +3,16 @@
 
 a = []
 
-puts "Please enter two numbers and I will revert them to you sorted in ascending order"
-puts "Please enter the first number"
+puts "Please enter two numbers and I will revert them to you sorted in ascending order. \nPlease enter the first number:"
 a.push(gets.to_i)
 puts "Please enter the second number"
 a.push(gets.to_i)
 
-if a[0] > a[1]
+result = if a[0] > a[1]
   number = a.pop
-  puts "Here you go: #{a.unshift(number).join(", ")}"
+    "Here you go: #{a.unshift(number).join(", ")}"
 else 
-  puts "Here you go: #{a.join(", ")}"
+    "Here you go: #{a.join(", ")}"
 end
+
+puts result
