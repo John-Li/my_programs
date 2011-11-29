8315507cb6983359c4a16d2503dcecb76a835e7c
John Li <ivan.lishchinovskiy@gmail.com>
2011-11-29 16:08:56 +0200

    task3 fixed

--- book/task3-1.rb
+++ book/task3-1.rb
@@ -1,18 +1,13 @@
 #Задача 1.
 #Даны три действительных числа X, Y, Z. Найти максимальное из этих трех чисел.
 
-puts "Please enter three numbers to find which is the biggest one"
-puts "Number 1 is:"
-a = gets.to_i
-puts "Number 2 is:"
-b = gets.to_i
-puts "Number 3 is:"
-c = gets.to_i
+a = []
 
-if a > b and a > c
-  puts "First number is the biggest"
-elsif b > a and b > c 
-  puts "Second number is the biggest"
-elsif c > a and c > b 
-  puts "Third number is the biggest"
-end
+puts "Please enter three numbers to find which is the biggest one. \nNumber 1 is:"
+a.push(gets.to_i)
+puts "Number 2 is:"
+a.push(gets.to_i)
+puts "Number 3 is:"
+a.push(gets.to_i)
+
+puts "This #{a.max} is the biggest one!"
