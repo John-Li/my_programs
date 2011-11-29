9e41818809143c35b723c4e2c8a20991f26dbf81
John Li <ivan.lishchinovskiy@gmail.com>
2011-11-29 14:29:45 +0200

    task4 modified

--- task4.rb
+++ task4.rb
@@ -1,14 +1,15 @@
 #Даны два числа. Упорядочить их двух значений по возрастанию
 #   использование условного оператора if
 
-puts "Please enter two numbers and I will revert them to you sorted in ascending order"
-puts "Please enter the first number"
+puts "Please enter two numbers and I will revert them to you sorted in ascending order. \nPlease enter the first number:"
 num1 = gets.to_i
 puts "Please enter the second number"
 num2 = gets.to_i
 
-if num1 > num2 
-  puts "Here you go: #{num2}, #{num1}"
+result = if num1 > num2 
+  "Here you go: #{num2}, #{num1}"
 else 
-  puts "Here you go: #{num1}, #{num2}"
+  "Your numbers is already in order, see: #{num1}, #{num2}"
 end
+
+puts result
