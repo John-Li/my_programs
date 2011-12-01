709a437da9736c84923c69719c94061ce56a8fdc
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-01 19:52:52 +0200

    should be_within expression corrected

--- book/specs/task-1-2_spec.rb
+++ book/specs/task-1-2_spec.rb
@@ -2,7 +2,7 @@ require '../task-1-2.rb'
 
 describe "task-1-2" do
   it "should find square of a truncated cone" do
-    square(20, 10, 30).should be_within(4548.8, 0.89)
+    square(20, 10, 30).should be_within(0.01).of(4548.8)
   end
   
   it "should find volume of a truncated cone" do
