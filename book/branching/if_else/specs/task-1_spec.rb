dbffe2a2bc9e1a285ca95cb4ca0ca4652c1be763
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-02 10:58:26 +0200

    task-1 and its spec added

---
+++ book/branching/if_else/specs/task-1_spec.rb
@@ -0,0 +1,7 @@
+require '../task-1.rb'
+
+describe "task-1" do
+  it "should find a maximum of 3 numbers" do
+    find_max(1, 2, 3).should == 3  
+  end
+end
---
+++ book/branching/if_else/task-1.rb
@@ -0,0 +1,7 @@
+def find_max(x, y, z)
+  a = []  
+  a << x << y << z  
+  a.max
+end
+
+
