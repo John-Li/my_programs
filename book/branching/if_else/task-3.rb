703b075bb815f5ea558864caeb869b1a7458c1be
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-02 14:27:17 +0200

    task-3 rafactored

--- book/branching/if_else/task-3.rb
+++ book/branching/if_else/task-3.rb
@@ -2,13 +2,8 @@ def multi(a, b, c)
   ary = [a, b, c]
   
   if a >= b and b >= c  
-    ary_a = []
-    ary.each {|i| ary_a << i*2}
-    ary_a
-  
+    ary.collect! {|i| i*2}
   else
-    ary_b = []
-    ary.each {|i| ary_b << i.abs}
-    ary_b
+    ary.collect! {|i| i.abs}
   end        
 end  
