cb67978b56bdd72372b8631e6514d98ee49402a7
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-02 13:30:47 +0200

    task-3 and its spec fixed

--- book/branching/if_else/specs/task-3_spec.rb
+++ book/branching/if_else/specs/task-3_spec.rb
@@ -4,4 +4,8 @@ describe "task-3" do
   it "shold double 3 numbers if num1 => num2 => num3" do
     multi(3, 2, 1).should == [6, 4, 2]
   end
+  
+  it "should change 3 numbers with its abs. meanings if num1 => num2 => num3 expression is not true" do
+    multi(-2, 3, -4).should == [2, 3, 4]  
+  end
 end
--- book/branching/if_else/task-3.rb
+++ book/branching/if_else/task-3.rb
@@ -1,8 +1,14 @@
 def multi(a, b, c)
-  ary = []
-  ary << a << b << c   
+  ary = [a, b, c]
+  
   if a >= b and b >= c  
-  ary = ary.each {|i| ary << i*2}
-  end
-else   
+    ary_a = []
+    ary.each {|i| ary_a << i*2}
+    ary_a
+  
+  else
+    ary_b = []
+    ary.each {|i| ary_b << i.abs}
+    ary_b
+  end        
 end  
