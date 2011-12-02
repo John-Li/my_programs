06c558abb7afef784b1b0340ab290d1b89fb17b0
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-02 15:05:06 +0200

    task-5 and its spec added

---
+++ book/branching/if_else/specs/task-5_spec.rb
@@ -0,0 +1,11 @@
+require '../task-5.rb'
+
+describe "task-5" do
+  it "should replace number 1 with 0 if num1 <= num2" do
+    rep(3, 6).should == "0"
+  end
+  
+  it "should leave unchanged number 1 and 2 if num1 <= num2 expression is false" do
+    rep(-3, -6).should == "-3, -6"
+  end
+end
---
+++ book/branching/if_else/task-5.rb
@@ -0,0 +1,7 @@
+def rep(a, b)
+  if a <= b
+    "#{a = 0}"
+  else 
+    "#{a}, #{b}"
+  end
+end
