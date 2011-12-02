ef37f4d8c666c1a5ad98a645d529a1a5374c83e0
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-02 17:05:05 +0200

    task-8 and its spec added *still need some changes*

---
+++ book/branching/if_else/specs/task-8_spec.rb
@@ -0,0 +1,7 @@
+require '../task-8'
+
+describe "task-8" do
+  it "should square all positive numbers of 3 given" do
+    method_8(1, 3, -3).should == "1, 9, -3"
+  end
+end
---
+++ book/branching/if_else/task-8.rb
@@ -0,0 +1,6 @@
+def method_8(a, b, c)
+  a**2 if a > 0
+  b**2 if b > 0
+  c**2 if c > 0
+  "#{a}, #{b}, #{c}"
+end
