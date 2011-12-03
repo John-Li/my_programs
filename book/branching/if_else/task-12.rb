4dd4642c5a38a14822b1b91fe8d0911f50ed302f
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-03 09:51:07 +0200

    task-12 modified

--- book/branching/if_else/task-12.rb
+++ book/branching/if_else/task-12.rb
@@ -1,11 +1,11 @@
 def method_12(a, b)
   ary = [a,b]
   
-  if ary[0] and ary[1] < 0
+  if (ary[0] < 0) && (ary[1] < 0)
     ary.collect {|i| i.abs}
-  elsif ary[0] < 0 or ary[1] < 0
+  elsif (ary[0] < 0) || (ary[1] < 0)
     ary.collect {|i| i*0.5}
-  elsif ary.none? {|i|i == (0.5 .. 2.0)} and ary[0] and ary[1] >= 0 
+  elsif ary.none? {|i|i == (0.5..2.0)} && (ary[0] >= 0) && (ary[1] >= 0) 
     ary.collect {|i| i/10.0}
   else
     ary  
