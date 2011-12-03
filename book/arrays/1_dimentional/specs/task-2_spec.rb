fcf6fbc628251684c9b6fc6559ad5d1f22e469e9
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-03 21:47:56 +0200

    task-2 and its spec added

---
+++ book/arrays/1_dimentional/specs/task-2_spec.rb
@@ -0,0 +1,8 @@
+require '../task-2.rb'
+
+describe "task-1" do
+  it "should find min in array of 10 numbers and its index" do
+    ary = [12, 23, 43, 34, 66, 26, 65, 64, 35, 3]    
+    method_2(ary).should == "3, 9"
+  end
+end
---
+++ book/arrays/1_dimentional/task-2.rb
@@ -0,0 +1,3 @@
+def method_2(array)
+  "#{array.min}, #{array.index(array.min)}"
+end
