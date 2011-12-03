339398f2b9c480861490fb9a057a85e7f88c1083
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-03 17:29:31 +0200

    task-3 and its spec added

---
+++ book/branching/case_operator/specs/task-3_spec.rb
@@ -0,0 +1,11 @@
+require '../task-3.rb'
+
+describe "task-3" do
+  it "should return verbal name according to mark given" do
+    method_3(4).should == "Khorosho"
+  end
+  
+  it "should return verbal name according to mark given" do
+    method_3(2).should == "Neudovletvoritel`no"
+  end
+end
---
+++ book/branching/case_operator/task-3.rb
@@ -0,0 +1,5 @@
+def method_3(mark)
+  name = {1 => "Ochen` plokho", 2 => "Neudovletvoritel`no", 3 => "Udovletvoritelno", 4 => "Khorosho", 5 => "Otlishno"}
+  
+  "#{name[mark]}"
+end
