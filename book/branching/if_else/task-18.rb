e05dd08868cda9ca0d1ff41db72e9ef3081f4c11
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-04 13:47:49 +0200

    task-18 finally fixed

--- book/branching/if_else/task-18.rb
+++ book/branching/if_else/task-18.rb
@@ -1,8 +1,11 @@
 def method_18(x, y) 
-  quarter = if (x > 0) && (y > 0) then 1
-         elsif (x > 0) && (y < 0) then 2
-         elsif (x < 0) && (y < 0) then 3
-         elsif (x < 0) && (y > 0) then 4
+  return "You cannot assign '0' to XY" if x.zero? || y.zero?
+
+  quarter = if x > 0
+    y > 0 ? 1 : 2
+  else
+    y > 0 ? 4 : 3
   end
+
   "it is quarter #{quarter}"
 end
