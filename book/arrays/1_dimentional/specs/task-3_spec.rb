3b0b11be3dbc953624eda8c1f09ca9967fe967c4
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-04 13:15:28 +0200

    task-3_spec modified

--- book/arrays/1_dimentional/specs/task-3_spec.rb
+++ book/arrays/1_dimentional/specs/task-3_spec.rb
@@ -2,7 +2,7 @@ require '../task-3.rb'
 
 describe "task-3" do
   it "should first negative in array of 10 numbers and its index" do
-    ary = [12, 23, 43, -34, 66, 26, 65, 64, 35, 3]    
+    ary = [12, 23, 43, -34, 66, -26, 65, 64, 35, 3]    
     method_3(ary).should == [-34, 3]
   end
 end
