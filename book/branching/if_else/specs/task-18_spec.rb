7b6284e4b600de9c93ac32135354eec149c90178
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-04 10:41:21 +0200

    task-18 corrected, still need 1 modification

--- book/branching/if_else/specs/task-18_spec.rb
+++ book/branching/if_else/specs/task-18_spec.rb
@@ -6,18 +6,18 @@ require '../task-18.rb'
 
 describe "task-18" do
   it "Should assign a number of quarter in which is point with X and Y coordinates situated" do
-    method_18(1, 5).should == "it is 1-st quarter"
+    method_18(1, 5).should == "it is quarter 1"
   end
 
   it "Should assign a number of quarter in which is point with X and Y coordinates situated" do
-    method_18(3, -7).should == "it is 2-nd quarter"
+    method_18(3, -7).should == "it is quarter 2"
   end
 
   it "Should assign a number of quarter in which is point with X and Y coordinates situated" do
-    method_18(-51, -70).should == "it is 3-rd quarter"
+    method_18(-51, -70).should == "it is quarter 3"
   end
   
   it "Should assign a number of quarter in which is point with X and Y coordinates situated" do
-    method_18(-15, 56).should == "it is 4-th quarter"
+    method_18(-15, 56).should == "it is quarter 4"
   end
 end
--- book/branching/if_else/task-18.rb
+++ book/branching/if_else/task-18.rb
@@ -1,11 +1,8 @@
-def method_18(x, y)  
-  if (0.0..+1.0/0.0).include?(x) & (0.0..+1.0/0.0).include?(y)
-    "it is 1-st quarter"
-  elsif (0.0..+1.0/0.0).include?(x) & (0.0..-1.0/0.0).include?(y)
-    "it is 2-nd quarter"
-  elsif (0.0..-1.0/0.0).include?(x) & (0.0..-1.0/0.0).include?(y)
-    "it is 3-rd quarter"
-  elsif (0.0..-1.0/0.0).include?(x) & (0.0..+1.0/0.0).include?(y)
-    "it is 4-th quarter"
+def method_18(x, y) 
+  quarter = if (x > 0) && (y > 0) then 1
+         elsif (x > 0) && (y < 0) then 2
+         elsif (x < 0) && (y < 0) then 3
+         elsif (x < 0) && (y > 0) then 4
   end
+  "it is quarter #{quarter}"
 end
