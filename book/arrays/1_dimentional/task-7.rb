71e33aae5e1d4f36b2f1e9d9024d028ac8eb1919
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-05 13:22:04 +0200

    task-7 modified

--- book/arrays/1_dimentional/task-7.rb
+++ book/arrays/1_dimentional/task-7.rb
@@ -1,4 +1,4 @@
 def method_7(array)
-  positiv_array = array.find_all {|i| i >= 0}
-  positiv_array.inject {|sum, i| sum + i}
+  positiv_array = array.find_all {|i| i > 0}
+  positiv_array.inject(0) {|sum, i| sum += i}
 end
