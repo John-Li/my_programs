33f8f22c9919875cb91cbff28bdbe4e4ca2079ef
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-05 19:18:07 +0200

    task-15 modified *second try, still don't working*

--- book/arrays/1_dimentional/task-15.rb
+++ book/arrays/1_dimentional/task-15.rb
@@ -1,8 +1,6 @@
 def method_15(array)
-  array_of_even = array.find_all {
-    |number| number == number.index {
-      |index| index == even?
-    }
-  }
+  array_of_even = array.delete {|number| index(number) != even? }
   array_of_even.inject(0) {|sum, number| sum += number }
 end
+
+
