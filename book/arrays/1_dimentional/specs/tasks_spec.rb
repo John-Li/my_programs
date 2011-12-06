073e0502c96c182f0cf055f3a33008834d7703a3
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-06 12:01:39 +0200

    task-15 and its spec added

---
+++ book/arrays/1_dimentional/specs/tasks_spec.rb
@@ -0,0 +1,20 @@
+require '../task-13.rb'
+require '../task-14.rb'
+require '../task-15.rb'
+
+describe "1-dimentional array specs" do
+  let(:array) { [12, 23, -3, 34, 0, 26, -65, 64, 35, -3] }
+  
+  it "should product of all negative numbers in range" do
+    method_13(array).should == -585
+  end  
+  
+  it "should find quantity of all elements in array bigger than 3" do
+    method_14(array).should == 6
+  end
+    
+  it "should find product of all elements with even index" do
+    method_15(array).should == -21
+  end
+  
+end
--- book/arrays/1_dimentional/task-15.rb
+++ book/arrays/1_dimentional/task-15.rb
@@ -1,6 +1,8 @@
+#Задача 15.
+#В массиве из 10 чисел подсчитать сумму элементов, стоящих на четных местах.
+
 def method_15(array)
-  array_of_even = array.delete {|number| index(number) != even? }
-  array_of_even.inject(0) {|sum, number| sum += number }
+  sum = 0
+  array.each_with_index {|number, index| sum += number if index.even? }
+  sum
 end
-
-
