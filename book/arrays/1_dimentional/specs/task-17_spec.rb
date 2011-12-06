6633479162e75a2ff2830c89743c4ece78622b92
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-06 13:57:06 +0200

    task-17 and its spec added *need some corrections* ' '

---
+++ book/arrays/1_dimentional/specs/task-17_spec.rb
@@ -0,0 +1,10 @@
+require '../task-17.rb'
+
+describe "task-16" do
+  let (:array) {[1,0,3,4,0,0,7,0,9,10]}
+  
+  it "should replace all zeros to the end of array" do
+    method_17(array).should == [1,3,4,7,9,10,0,0,0,0]
+  end
+end
+
---
+++ book/arrays/1_dimentional/task-17.rb
@@ -0,0 +1,11 @@
+# Задача 17.
+# В массиве из 10 чисел переставить все нули в конец массива, не меняя
+# порядок не нулевых элементов.
+# Контрольный пример: исходный массив X =[1,0,3,4,0,0,7,0,9,10]   
+# Результат: полученный массив X =[1,3,4,7,9,10,0,0,0,0].
+
+def method_17(array)
+  zero_array = []    
+  array.each_with_index {|number, index|zero_array << array.slice!(index) if number.zero?}
+  array.concat(zero_array)
+end
