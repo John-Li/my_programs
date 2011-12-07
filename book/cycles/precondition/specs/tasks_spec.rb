a9a04e69c6e50599fd0ccac0c74f625f251c9269
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-07 22:11:15 +0200

    task-1 fixed, specs updated

--- book/cycles/precondition/specs/tasks_spec.rb
+++ book/cycles/precondition/specs/tasks_spec.rb
@@ -4,7 +4,7 @@ require '../task-2.rb'
 describe "cycles with precondition" do
 
   it "should raise number into degree" do
-    method_1(4,2).should == 16
+    method_1(2,3).should == 8
   end
   
   it "should find factorial of number given" do
--- book/cycles/precondition/task-1.rb
+++ book/cycles/precondition/task-1.rb
@@ -2,9 +2,13 @@
 # С помощью цикла «пока» или цикла «до» написать программу
 # возведения числа A в целую степень N.
 
-def method_1(number)
-  until number == 16 do 
-    number *= number
+def method_1(number, power)
+  result = 1  
+  for i in 1..power
+    result = result * number        
   end
-  number
+  result
 end
+
+
+
