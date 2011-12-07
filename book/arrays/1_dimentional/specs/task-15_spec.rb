6aa26d4a50b8f9a8bc86ed2218ba38d2a983f34a
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-07 22:15:37 +0200

    task-15_spec updated

--- book/arrays/1_dimentional/specs/task-15_spec.rb
+++ book/arrays/1_dimentional/specs/task-15_spec.rb
@@ -1,11 +1,8 @@
-#Задача 15.
-#В массиве из 10 чисел подсчитать сумму элементов, стоящих на четных местах.
-
 require '../task-15.rb'
 
 describe "task-15" do
   it "should find product of all elements with even index" do
     array = [12, 23, -3, 34, 0, 26, -65, 64, 35, -3]
-    method_15(array).should == -33
+    method_15(array).should == -21
   end
 end
