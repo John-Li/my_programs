e58c1acc1e221cec70a57a1ba4e7e9c191046fe9
John Li <ivan.lishchinovskiy@gmail.com>
2011-12-08 14:06:19 +0200

    task-6 added, specs updated

--- book/cycles/precondition/specs/tasks_spec.rb
+++ book/cycles/precondition/specs/tasks_spec.rb
@@ -2,6 +2,7 @@ require '../task-1.rb'
 require '../task-2.rb'
 require '../task-3.rb'
 require '../task-4.rb'
+require '../task-6.rb'
 
 describe "cycles with precondition" do
 
@@ -17,8 +18,11 @@ describe "cycles with precondition" do
     method_3(4).should == 5
   end
 
-  it "should find sum of number squared" do
+  it "should find sum of number squared in 1..N range" do
     method_4(4).should == 30
   end
 
+  it "should find sum of even squared and odd cubed in 1..N range" do
+    method_6(4).should == 48
+  end
 end
